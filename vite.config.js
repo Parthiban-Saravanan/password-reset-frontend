0e79db92155a1fa1bb871b872ee1b40e812ecb0b
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-12 04:29:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://password-reset-backend-n4xb.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
